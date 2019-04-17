72006d3317635b1d611ca354dda85a564fe315a2
haeejuut <haeejuut@student.jyu.fi>
2019-04-17 12:17:39 +0300

    pygame_pong executable

--- Sykli 1 Pelimoottorit/pygame_pong/setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("pygame_pong.py")]
-
-cx_Freeze.setup(
-    name="pong",
-    options={"build_exe": {"packages": "pygame"}},
-    executables=executables
-    )
