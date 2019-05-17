ad7ece2833e5f70e3a5eca51f2f3b99fbd1e9a6e
haeejuut <haeejuut@student.jyu.fi>
2019-05-17 09:53:02 +0300

    korjaukset

--- Sykli 8 Pelitestaus/PongUnity/Assets/TestPlay/TestPlay.cs
+++ Sykli 8 Pelitestaus/PongUnity/Assets/TestPlay/TestPlay.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.TestTools;
 
 namespace Tests
@@ -20,9 +20,37 @@ namespace Tests
         [UnityTest]
         public IEnumerator TestPlayWithEnumeratorPasses()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return new WaitForSeconds(5);
+            // ladataan skene ja odotetaan sen latausta puoli sekuntia
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            yield return new WaitForSeconds(1);
+
+            // CASE 1
+            // Pistetekstien luominen
+            var pogo = GameObject.Find("Points");
+            var potext = pogo.GetComponent<Text>();
+            Assert.AreEqual("0", potext.text);
+
+            var cogo = GameObject.Find("ComputerPoints");
+            var cotext = cogo.GetComponent<Text>();
+            Assert.AreEqual("0", cotext.text);
+
+            // Onko tekstit samat
+            Assert.AreEqual(potext.text, cotext.text);
+
+            // CASE 2
+            // Tarkistetaan onko pelaamiseen tarvitut objektit olemassa
+            var maila = GameObject.Find("Player");
+            var maila2 = GameObject.Find("Computer");
+            var pallo = GameObject.Find("Ball");
+            var seinat = GameObject.FindGameObjectsWithTag("seina");
+
+            Assert.IsNotNull(maila, "Pelaajan mailaa ei löytynyt");
+            Assert.IsNotNull(maila2, "AI:n mailaa ei löytynyt");
+            Assert.IsNotNull(pallo, "Palloa ei löytynyt");
+            foreach (var s in seinat)
+            {
+                Assert.IsNotNull(s, "Seinä puuttuu");
+            }
         }
     }
 }
